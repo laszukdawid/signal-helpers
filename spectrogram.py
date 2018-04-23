31391f33de223ed853f480c637cd27f2a937a838
Dawid Laszuk <laszukdawid@gmail.com>
2018-04-22 22:35:16 -0700

    Spectrogram example

---
+++ spectrogram.py
@@ -0,0 +1,41 @@
+"""
+Example of using spectrogram.
+
+More on the spectrogram:
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.spectrogram.html
+"""
+import numpy as np
+import pylab as plt
+from scipy.signal import chirp
+from scipy.signal import spectrogram
+
+# Define signal
+dt = 0.0001
+time_array = np.arange(0, 2, dt)
+signal = chirp(time_array, 10.0, time_array[-1], 40.0)
+signal *= (np.cos(2*np.pi*time_array) + 1.2)
+
+# Calculate spectrogram
+fs = 1./dt
+window = 'hanning'
+nperseg = 256  # samples per segment
+noverlap = nperseg // 8  # samples overlapped
+
+freq, t, spectr = spectrogram(signal, fs, window, nperseg=nperseg, noverlap=noverlap)
+
+# Display results
+fig, axes = plt.subplots(2)
+
+ax = axes[0]
+ax.plot(time_array, signal)
+
+print(t[0], t[-1])
+ax = axes[1]
+ax.imshow(spectr, aspect='auto',
+            extent=[t[0], t[-1], freq[0], freq[-1]],
+            origin='lower',
+            vmin=spectr.min(), vmax=spectr.max(),
+            )
+
+plt.tight_layout()
+plt.show()
